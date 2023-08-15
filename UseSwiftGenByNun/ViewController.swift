8bf8879fc4aae101bbacf30e68e9f4c93ecc9dbd
Apinun <apinun@Apinuns-MacBook-Pro-2.local>
2023-08-15 16:10:49 +0700

    fist install

---
+++ UseSwiftGenByNun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UseSwiftGenByNun
+//
+//  Created by Apinun on 15/8/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
